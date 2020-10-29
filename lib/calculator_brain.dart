6586ae64ff46fbee9036f5f852be8eefb35ad6e4
DevYuns <ydhun93@gmail.com>
2020-10-29 18:27:30 +0900

    Routes and composition etc

---
+++ lib/calculator_brain.dart
@@ -0,0 +1,35 @@
+import 'dart:math';
+
+class CalculatorBrain {
+  CalculatorBrain({this.weight, this.height});
+
+  final int height;
+  final int weight;
+
+  double _bmi;
+
+  String calculateBMI() {
+    _bmi = weight / pow(height / 100, 2);
+    return _bmi.toStringAsFixed(1); // 소숫점이 많은 숫자를 스트링으로 변환할때 자리수를 받아서 바로 변환해준다.
+  }
+
+  String getResult() {
+    if (_bmi >= 25) {
+      return 'Overweight';
+    } else if (_bmi > 18.5) {
+      return 'Normal';
+    } else {
+      return 'UnderWeight';
+    }
+  }
+
+  String getInterpretation() {
+    if (_bmi >= 25) {
+      return 'You have a higher than normal body weight. Try to excercise more.';
+    } else if (_bmi > 18.5) {
+      return 'You have a normal body weight. Good job!';
+    } else {
+      return 'You have a lower than normal body weight. You can eat a bit more';
+    }
+  }
+}
---
+++ lib/screens/results_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+import '../components/reusable_card.dart';
+import '../components/bottom_button.dart';
+
+class ResultsPage extends StatelessWidget {
+  ResultsPage({
+    @required this.bmiResult,
+    @required this.interpretation,
+    @required this.resultsText,
+  });
+
+  final String bmiResult;
+  final String resultsText;
+  final String interpretation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('BMI CALCULATOR')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                resultsText,
+                style: kTitleTextStyle,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    resultsText.toUpperCase(),
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    bmiResult,
+                    style: kBMITextStyle,
+                  ),
+                  Text(
+                    interpretation,
+                    textAlign: TextAlign.center,
+                    style: kBodyTextStyle,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          BottomButton(
+            buttonTitle: 'RE-CALCULATE',
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
